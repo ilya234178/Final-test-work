b9dde630b2d5cf0f7a2312e3c877414864d2f282
Ilya Veryuhanov <veruhanov00@mail.ru>
2022-09-05 14:08:01 +0400

    added info Readme , test.drawio, Program

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿
+Console.WriteLine("Введите массив строк через пробел");
+string stroka = Console.ReadLine();
+string[] array = stroka.Split(" ");
+var result = new string[array.Length];
+var realSize = 0;
+foreach (var value in array)
+{
+    if (value.Length <= 3)
+    {
+        result[realSize] = value;
+        realSize++;
+    }
+}
+Console.WriteLine(string.Join(Environment.NewLine, result, 0, realSize));
+Console.ReadKey(true);
